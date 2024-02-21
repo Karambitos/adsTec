daef1a62a1ef0f3ac745e639e04faf5a16a477f7
Dmytro Pasichnyk <pase4nikdv@gmail.com>
2024-02-21 17:36:02 +0100

    added iv test

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-(function ($) {
-    "use strict";
-    $(document).ready(function () {
-        console.log('App.js');
-    });
-})(jQuery);
+const accordions = document.querySelectorAll('.accordion')
+
+accordions.forEach((accordion) => {
+accordion.addEventListener('click', () => {
+    const body = accordion.querySelector('.accordion-body')
+    body.classList.toggle('active')
+})
+})
